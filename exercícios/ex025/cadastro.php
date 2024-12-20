356b53a187dc4bc5a0d06d7bbe29dc562713fdf8
Roberto Carvalho <155508919+sublinhe@users.noreply.github.com>
2024-12-20 18:42:23 -0300

    exercício 001 e 002 de formulário

---
+++ exercícios/ex025/cadastro.php
@@ -0,0 +1,3 @@
+Os dados foram enviados. Fique tranquilo.
+De todo modo, infelizmente não irei te falar para oque serve esse dados e como funciona o envio deles.
+Se quiser aprender, Por favor, faça um curso de PHP
