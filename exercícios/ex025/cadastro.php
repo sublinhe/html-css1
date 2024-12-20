747904700d57ced10d32accaa4dc1a3b4f18c27c
Roberto Carvalho <155508919+sublinhe@users.noreply.github.com>
2024-12-20 19:52:12 -0300

    deletei cadastro.php

--- exercícios/ex025/cadastro.php
+++
@@ -1,3 +0,0 @@
-Os dados foram enviados. Fique tranquilo.
-De todo modo, infelizmente não irei te falar para oque serve esse dados e como funciona o envio deles.
-Se quiser aprender, Por favor, faça um curso de PHP
